84f0775b862a39175a3b1b600341578ec55569d4
Pavel Sukhodolskii <pvlt.job@gmail.com>
2015-11-19 18:45:55 +0300

    Добавлен веер кнопок

--- script.js
+++ script.js
@@ -107,15 +107,38 @@
             d3.select(self.frameElement).style("height", "800px");
 
 
-
         },
         update: function(source) {
+            var data = [{
+                "name": "1"
+            }, {
+                "name": "2"
+            }, {
+                "name": "3"
+            }, {
+                "name": "4"
+            }, {
+                "name": "5"
+            }, {
+                "name": "6"
+            }, {
+                "name": "7"
+            }, {
+                "name": "8"
+            }, {
+                "name": "9"
+            }]
+            var gwidth = 70,
+                gheight = 70;
+            g = svg.append('g').attr("transform", "translate(500,700)").attr("width", gwidth)
+                .attr("height", gheight);
+            var n = data.length;
+
             var ctl = this;
             var nodes = tree.nodes(root).reverse(),
                 links = tree.links(nodes);
 
             nodes.forEach(function(d) {
-                console.log(d);
                 d.y = d.depth * 300;
             });
 
@@ -128,7 +151,7 @@
                 .attr("class", "node")
                 .attr('id', function(d, i) {
                     return 'node-' + i;
-                })
+                });
 
             nodeEnter.append("circle")
                 .attr("r", 30)
@@ -145,8 +168,8 @@
                 alias: 'widget.win',
                 itemId: 'win',
                 title: 'Окошко',
-                x:200,
-                y:200,
+                x: 200,
+                y: 200,
                 width: 300,
                 height: 300,
                 text: '',
@@ -159,18 +182,24 @@
                 draggable: true,
                 closable: true,
                 modal: false,
-                items:[{xtype:'displayfield'}],
-                headerPosition: 'top',renderTo: 'ext-zone'
+                items: [{
+                    xtype: 'displayfield'
+                }],
+                headerPosition: 'top',
+                renderTo: 'ext-zone'
             });
-            win = Ext.create('app.window')
+            win = Ext.create('app.window');
             d3.selectAll('.circle')
                 .each(function(e, i) {
+                    d3.select('#circle-' + i).on('dblclick', function(e, t, eOpts) {
+                        console.log('dblclick');
+                    });
                     d3.select('#circle-' + i).on('click', function(e, t, eOpts) {
                         d3.select('#circle-' + i)
                             .transition()
                             .duration(100)
                             .attr("transform", function(d) {
-                                return "scale(1.2, 1.2)";
+                                return "scale(0.9, 0.9)";
                             })
                             .transition()
                             .duration(100)
@@ -182,43 +211,84 @@
                             if (!d3.select('#button-' + i)[0][0]) {
                                 d3.select('#circle-' + i)
                                     .classed('unselected', false)
-                                    .classed('selected', true)
+                                    .classed('selected', true);
                                 this.pressed = true;
                                 if (d3.selectAll('.selected').size() == 1)
                                     ctl.prevPressed = i;
                                 if (d3.selectAll('.selected').size() > 1) {
                                     d3.select('#circle-' + ctl.prevPressed).classed('unselected', true).classed('selected', false).attr('r', 30).property('pressed', null);
-                                    d3.select('#button-' + ctl.prevPressed).transition().duration(200).attr("opacity", 0).remove();
+                                    d3.selectAll('.button').transition().duration(1000).attr("opacity", 0).remove();
                                     ctl.prevPressed = i;
                                 }
-                                d3.select('#node-' + i).append("circle")
-                                    .attr("id", function(d) {
-                                        return "button-" + i;
-                                    })
-                                    .attr('r', r)
-                                    .attr("transform", function(d) {
-                                        return "translate(" + (50 + r + 5 + 10) + ", 0)";
-                                    })
-                                    .attr("opacity", 0)
-                                    .transition()
-                                    .duration(200)
-                                    .attr("opacity", 1)
-                                    .style("fill", function(d) {
-                                        return d._children ? "lightsteelblue" : "#fff";
+                                d3.select('#node-' + i).attr("width", gwidth)
+                                    .attr("height", gheight);
+                                data.forEach(function(d, j) {
+                                    var sector = j * 360 / n,
+                                        radius = n * gwidth / (n + 2),
+                                        δy = n & 1 ? (radius / 2) * (1 - Math.cos(Math.PI / n)) : 0;
+                                    d3.select('#node-' + i).append('circle').attr("r", r).attr("transform", function(d) {
+                                            var sector = j * 360 / n;
+                                            return "translate(0," + δy + ")rotate(" + sector + ")translate(" + radius + ")rotate(" + -sector + ")";
+                                        }).attr("id", function(d) {
+                                            return "button-" + i + j;
+                                        })
+                                        .classed('button', true)
+                                        .attr("opacity", 0)
+                                        .transition()
+                                        .duration(200)
+                                        .attr("opacity", 1)
+                                        .style("fill", function(d) {
+                                            return d._children ? "lightsteelblue" : "#fff";
+                                        })
+                                    d3.select('#button-' + i + j).on('click', function(e, t, eOpts) {
+                                        console.log(e)
+                                        var attr = d3.select(this).attr("transform");
+                                        d3.select(this).transition()
+                                            .duration(100)
+                                            .attr("transform", function(d) {
+                                                return attr + "scale(0.9, 0.9)";
+                                            })
+                                            .transition()
+                                            .duration(100)
+                                            .attr("transform", function(d) {
+                                                return attr + "scale(1, 1)";
+                                            });
+                                        win.show();
+                                        win.down('displayfield').setValue(Ext.JSON.encode(d3.select('#node-' + i).data()[0].data[0]));
                                     });
-                                Ext.get('button-' + i).on('click', function(e, t, eOpts) {
+                                    d3.select('#button-' + i + j).on('mouseenter', function(e, t, eOpts) {
+                                        d3.select('#button-' + i + j).transition()
+                                            .duration(100).attr('r', 2 * r);
+                                    });
+                                    d3.select('#button-' + i + j).on('mouseleave', function(e, t, eOpts) {
+                                        d3.select('#button-' + i + j).transition()
+                                            .duration(100).attr('r', r);
+                                    });
+
+
+                                });
+                                d3.select('#button-' + i).on('click', function(e, t, eOpts) {
+                                    console.log(e)
                                     d3.select('#button-' + i).transition()
                                         .duration(100)
                                         .attr("transform", function(d) {
-                                            return "translate(" + (50 + r + 5 + 10) + ", 0) scale(1.2, 1.2)";
+                                            return "translate(" + (50 + r + 5) + ", 0) scale(0.9, 0.9)";
                                         })
                                         .transition()
                                         .duration(100)
                                         .attr("transform", function(d) {
-                                            return "translate(" + (50 + r + 5 + 10) + ", 0) scale(1, 1)";
+                                            return "translate(" + (50 + r + 5) + ", 0) scale(1, 1)";
                                         });
-                                          win.show();
-                                          win.down('displayfield').setValue(Ext.JSON.encode(d3.select('#node-' + i).data()[0].data[0]));
+                                    win.show();
+                                    win.down('displayfield').setValue(Ext.JSON.encode(d3.select('#node-' + i).data()[0].data[0]));
+                                });
+                                d3.select('#button-' + i).on('mouseenter', function(e, t, eOpts) {
+                                    d3.select('#button-' + i).transition()
+                                        .duration(100).attr('r', 2 * r);
+                                });
+                                d3.select('#button-' + i).on('mouseleave', function(e, t, eOpts) {
+                                    d3.select('#button-' + i).transition()
+                                        .duration(100).attr('r', r);
                                 });
                             }
                         } else {
@@ -226,7 +296,7 @@
                                 .classed('unselected', true)
                                 .classed('selected', false)
                                 .attr('r', 30);
-                            d3.select('#button-' + i)
+                            d3.selectAll('.button')
                                 .transition()
                                 .duration(200)
                                 .attr("opacity", 0)
@@ -239,17 +309,13 @@
             this.node = nodeEnter;
 
             nodeEnter.append("text")
-                .attr("x", function(d) {
-                    return d.children || d._children ? -10 : 10;
-                })
-                .attr("dy", ".35em")
-                .attr("text-anchor", function(d) {
-                    return d.children || d._children ? "end" : "start";
-                })
+                .attr("x", "-70")
+                // .attr("dy", ".35em")
+                .attr("text-anchor", 'middle')
                 .text(function(d) {
                     return d.name;
                 })
-                .style("fill-opacity", 1e-6);
+                // .style("fill-opacity", 1e-6);s
 
             var nodeUpdate = node.transition()
                 .duration(duration)
